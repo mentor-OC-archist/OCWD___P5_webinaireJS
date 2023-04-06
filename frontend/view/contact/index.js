b7023da9542005c70f9257383eef3a5498a268de
archist <mentor03.archist@gmail.com>
2023-04-06 15:47:34 +0200

    REBASE TO SCRATCH à partir du 1er commit, les consignes présents dans les différents fchiers ont étés remis à l'état final

--- frontend/view/contact/index.js
+++ frontend/view/contact/index.js
@@ -12,38 +12,49 @@ regex2text: https://www.browserling.com/tools/text-from-regex
 */
 
 /*
-pour tous les input et textarea, rajouter un "id", et compléter par un attribut "for" les balises label associés 
-pour tous les input et textarea, rajouter un required
-pour tous les input et textarea, rajouter un placeholder
-pour noms, rajouter un title 
-pour  noms, rajouter un attribut pattern, et y mettre une regex que vous créerez pour vérifier "contient au moins 2 mots séparés par une virgule suivi d'un espace: ', '"
-pour  message, rajouter deux attributs "minlength" et "maxlength" à respectivement 10 et 5000 sur message
-OPTION A) pour age, créer une regex pour vérifier "compris entre 18 et 120", et placer la dans un attribut pattern, puis rajouter un title décrivant cette regex
-OPTION B) pour age, rajouter deux attribut "min" et "max" à l'age
+importer "index.js" dans "index.html" avec l'attribut "defer"
+
+
+
+pour tous les input et textarea, rajouter un "id", et compléter par un attribut "for" les balises label associées 
+pour tous les input et textarea, rajouter un attribut "required"
+pour tous les input et textarea, rajouter un attribut "placeholder"
+pour l'input "noms", rajouter un title 
+
 pour tous les input, en css rajouter des regles css, via les pseudo-formats ":valid", ":invalid", et ":placeholder-shown"
 pour le textarea, en css rajouter dans les sélecteurs des règles précédentes un cas pour le textarea mais avec un nom de classe plutot que les pseudo-formats
 
-importer "index.js" dans "index.html" avec l'attribut "defer"
-puis pour message, rajouter un event "change", dedans: 
+pour l'input "noms", rajouter un attribut "pattern", et y mettre une regex que vous créerez pour vérifier "contient au moins 2 mots, dont le 1er est en majuscule et le 2nd est Capitalisé, séparés par une virgule suivi d'un espace: 'TEST, Prenom'"
+pour l'input "message", rajouter deux attributs "minlength" et "maxlength" aux valeurs respectives 10 et 5000 sur message
+OPTION A) pour l'input "age", créer une regex pour vérifier "compris entre 18 et 120", et placer la dans un attribut pattern, puis rajouter un title décrivant cette regex
+OPTION B) pour l'input "age", rajouter deux attribut "min" et "max"
+
+
+
+puis pour l'input "message", rajouter un event "change", dedans: 
     créer une variable contenant une regex pour "ne contient aucun caractère spéciaux"
-    tester la regex sur la "value" du textarea message: 
+    tester la regex sur la "value" du textarea "message": 
         si la réponse est false: 
             rajouter lui une classe d'échec via classList (et lui enlever toute autre classe de validation)
-            et utiliser la methode DOM .setCustomValidity("message d'erreur") sur message
+            et utiliser la methode DOM .setCustomValidity("message d'erreur") sur le textarea "message"
         si la réponse est true
             rajouter lui une class de succès via classList (et lui enlever toute autre classe de validation)
-            et utiliser la methode DOM .setCustomValidity("") sur message pour annuler l'erreur (potentiellement déclenchée en cas d'un 1er false)
+            et utiliser la methode DOM .setCustomValidity("") sur le textarea "message" pour annuler l'erreur (potentiellement déclenchée en cas d'un 1er false)
+
+
+
 pour le formulaire, rajouter un event "submit", dedans: 
     enlever le comportement par défaut du formulaire
-    créer une variable contenant un objet pour l'instant vide
-
-    faite un log de la l'objet DOM validity, puis de la propriété DOM validity.valid
-
+    créer une variable contenant un objet, dans lequel vous créez une(1) clé "datas" étant un objet  pour l'instant vide
+    faite un log de la l'objet DOM validity sur le textarea "message", puis de sa propriété DOM validity.valid
     si validity.valid est true, rediriger la page vers google xD
-        récupérer toutes les valeurs du formulaire, pour les insérer dans l'objet créé précédement
-            chaque valeur d'input associé au name de l'input
-        fetcher ces données en POST vers le endpoint /api/contact
-            afficher dans un alert la confirmation reçu, accompagné du message de confirmation reçu du backend.
+        récupérer toutes les valeurs du formulaire, pour les insérer à la clé "datas" de l'objet créé précédement
+            chaque valeur d'input associée à la valeur de l'attribut "name" de cette l'input
+        puis, fetcher ces données en POST vers le endpoint "/api/contact"
+            à la reception de la réponse du serveur(le backend) de cette requête, afficher dans un alert() le message de confirmation reçu (clé "confirmID"), ou le message d'erreur reçu (clé "message")'
+    sinon ....ne rien faire
+
+
 
 enfin, utiliser des outils de bundler, comme webpack ou parcel pour ...bundler votre application (càd l'optimiser grâce à ces outils)
     avec webpack:
@@ -58,7 +69,6 @@ enfin, utiliser des outils de bundler, comme webpack ou parcel pour ...bundler v
     avec parcel:
         lancer juste la commande: npx parcel frontend/view/home/index.html
         puis ouvrir une page sur le browser à l'adresse: http://localhost:1234
-
 */
 
 
